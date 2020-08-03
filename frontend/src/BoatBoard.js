4a24fb1b177579b9ac9804323013b0b59d368291
unknown <colleentenhave@gmail.com>
2020-08-03 16:41:40 -0700

    create boat board class and render beneath header

---
+++ frontend/src/BoatBoard.js
@@ -0,0 +1,41 @@
+import Board from 'react-trello';
+import React, { Component } from 'react';
+
+class BoatBoard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const data = {
+        lanes: [
+          {
+            id: 'lane-1',
+            title: 'Docked',
+            cards: []
+          },
+          {
+            id: 'lane-2',
+            title: 'Outbound to Sea',
+            cards: []
+          },
+          {
+            id: 'lane-3',
+            title: 'Inbound to Harbour',
+            cards: []
+          },
+          {
+            id: 'lane-4',
+            title: 'Maintenance',
+            cards: []
+          },
+        ]
+    }
+      return (
+        <div className="board">
+          <Board data={data} editable={true} />
+        </div>
+      );
+  }
+}
+export default BoatBoard;
